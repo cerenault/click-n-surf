e56f69c89100dd5ed8705ad919ae0e369558ff2f
cerenault <cesar.renault@etudiant.univ-lr.fr>
2022-03-03 14:47:10 +0100

    add docker-compose and improve main.py

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
+import fastapi
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world, welcome fastapi !"}
 
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port="8000", reload=True)
